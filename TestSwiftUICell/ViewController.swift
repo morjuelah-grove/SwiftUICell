8e6dcc3fac27b4ff55d590d1b4ce7135c2cc1b56
Miguel Orjuela <miguel.orjuela@grove.co>
2024-05-30 16:31:52 -0500

    Initial Commit

---
+++ TestSwiftUICell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSwiftUICell
+//
+//  Created by Miguel Orjuela on 30/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
